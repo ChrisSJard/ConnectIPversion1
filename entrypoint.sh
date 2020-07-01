f6c68177ad3f4724528b9076d662bcff71ac7838
Jervell <jervellthomas@gmail.com>
2020-07-01 18:48:51 +0100

    added support for S3 bucket

---
+++ entrypoint.sh
@@ -0,0 +1,4 @@
+python3 manage.py makemigrate
+python3 manage.py migrate
+#python3 manage.py collectstatic --no-input doesn't work only collect static files inside of apps, eg 
+python3 manage.py runserver 0.0.0.0:8000
