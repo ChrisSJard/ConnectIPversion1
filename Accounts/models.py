75cf77f64b4392a544f4515fd0032eb19dfaddbf
ChrisSJard <christian.s.jardine@gmail.com>
2020-08-07 23:57:56 +0100

    hotfix for dockerpush

--- Accounts/models.py
+++ Accounts/models.py
@@ -2,16 +2,12 @@ from django.db import models
 from django.contrib.auth.models import User
 from django_countries.fields import CountryField
 from ConnectIP.storage_backends import PrivateMediaStorage
-S3 = False
-if S3:
-    uploader = "upload_to='profile_pics'"
-else:
-    uploader = "storage=PrivateMediaStorage()"
+
 
 
 class Profile(models.Model):
     account = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='default.jpg', upload_to='profile_pics')  #change for production s3 storage
+    image = models.ImageField(default='default.jpg', storage=PrivateMediaStorage())
     qualification = models.CharField(max_length=100)
     research = models.CharField(max_length=100)
     location = CountryField()
