7eaf934c0b5edf483d96afc9356e668e656066f0
Jervell <jervellthomas@gmail.com>
2020-08-08 03:09:40 +0100

    Mistake in FILE STORAGE DEFAULT

--- ConnectIP/settings.py
+++ ConnectIP/settings.py
@@ -160,7 +160,7 @@ if USE_S3:
     # s3 public media settings
     PUBLIC_MEDIA_LOCATION = 'media'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
-    DEFAULT_FILE_STORAGE = 'hello_django.storage_backends.PrivateMediaStorage'
+    DEFAULT_FILE_STORAGE = 'ConnectIP.storage_backends.PublicMediaStorage'
 else:
     STATIC_URL = '/static/'
     STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
